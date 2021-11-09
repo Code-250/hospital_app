32ef86648c47f41301f2a408cb313cfacb2c89cb
Code-250 <richmunye@gmail.com>
2021-11-09 12:44:30 +0200

    first_mobile_app

---
+++ app/src/main/java/com/example/hospital_management_system_app/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.hospital_management_system_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qouramobileapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
