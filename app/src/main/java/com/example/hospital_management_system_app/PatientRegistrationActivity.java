4c0388ed516d48b26fca2c64b910cdee0ee888fd
Code-250 <richmunye@gmail.com>
2021-11-12 18:10:47 +0200

    registration pages

---
+++ app/src/main/java/com/example/hospital_management_system_app/PatientRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.hospital_management_system_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_registration);
+    }
+}
